6c9e7c9e53261e44494139588d50a6c710b1c6a0
Ambatidileep <dambati@cbu.edu>
2024-02-24 11:31:55 -0600

    hp_4.py
    Completed

--- src/hp_4.py
+++ src/hp_4.py
@@ -1,66 +1,38 @@
 # hp_4.py
-
-from datetime import datetime, timedelta
-from csv import DictReader, DictWriter
-from collections import defaultdict
-
-
-def reformat_dates(old_dates):
-    new_dates = []
-    for date_str in old_dates:
-        dt = datetime.strptime(date_str, '%Y-%m-%d')
-        new_dates.append(dt.strftime('%d %b %Y'))
-    return new_dates
-
-
-def date_range(start, n):
-    if not isinstance(start, str):
-        raise TypeError("start should be a string")
-    if not isinstance(n, int):
-        raise TypeError("n should be an integer")
-    start_date = datetime.strptime(start, '%Y-%m-%d')
-    return [start_date + timedelta(days=i) for i in range(n)]
-
-
-def add_date_range(values, start_date):
-    dates = date_range(start_date, len(values))
-    return list(zip(dates, values))
+import os
+import csv
+from datetime import datetime
 
 
 def fees_report(infile, outfile):
-    pass
+    if not os.path.exists(infile):
+        raise FileNotFoundError(f"Input file '{infile}' not found.")
+    late_fees_dict = {}
 
-import pytest
-from src.hp_4 import reformat_dates, date_range, add_date_range, fees_report
+    with open(infile, 'r', newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            date_due = datetime.strptime(row['date_due'], '%m/%d/%Y')
+            date_returned = datetime.strptime(row['date_returned'], '%m/%d/%Y')
+            if date_returned > date_due:
+                days_late = (date_returned - date_due).days
+                late_fee = days_late * 0.25
+            else:
+                late_fee = 0.0
+            patron_id = row['patron_id']
+            if patron_id in late_fees_dict:
+                late_fees_dict[patron_id] += late_fee
+            else:
+                late_fees_dict[patron_id] = late_fee
+    with open(outfile, 'w', newline='') as csvfile:
+        fieldnames = ['patron_id', 'late_fees']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
+        writer.writeheader()
+        for patron_id, late_fee in late_fees_dict.items():
+            writer.writerow({'patron_id': patron_id, 'late_fees': '{:.2f}'.format(late_fee)})
 
-def test_reformat_dates():
-    old_dates = ['2000-01-01', '2000-01-02', '2000-01-03']
-    expected_dates = ['01 Jan 2000', '02 Jan 2000', '03 Jan 2000']
-    assert reformat_dates(old_dates) == expected_dates
+infile = 'C:\\Users\\ambat\\Documents\\GitHub\\homework-project-4-Ambatidileep\\tests\\fixtures\\book_returns_short.csv'
+outfile = 'fees_report_out.csv'
+fees_report(infile, outfile)
 
-
-def test_date_range():
-    start = '2000-01-01'
-    n = 3
-    expected_dates = [
-        datetime(2000, 1, 1),
-        datetime(2000, 1, 2),
-        datetime(2000, 1, 3)
-    ]
-    assert date_range(start, n) == expected_dates
-
-
-def test_add_date_range():
-    values = [100, 101, 102]
-    start_date = '2000-01-01'
-    expected_result = [
-        (datetime(2000, 1, 1), 100),
-        (datetime(2000, 1, 2), 101),
-        (datetime(2000, 1, 3), 102)
-    ]
-    assert add_date_range(values, start_date) == expected_result
-
-
-def test_fees_report():
-    pass
